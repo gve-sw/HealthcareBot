a1f2e496a641b321bc639a36dc3f4c2d56bdc710
agabdelb <abdellbar@gmail.com>
2019-07-13 11:30:55 +0200

    redploying

--- server.py
+++ server.py
@@ -2,27 +2,35 @@
 from flask import Flask, request, abort
 from webexteamssdk import WebexTeamsAPI
 import json
+import os
 
 app = Flask(__name__)
 api_webexTeams = WebexTeamsAPI()
+BOT_PERSON_ID = os.environ['HOME']
 
 @app.route('/webhook',methods=['POST'])
 def webhook():
   if request.method == 'POST':
-      print('holla')
       incoming_message  = request.json
       #parse post reqest for message id and room id
       inc_msg_id  = incoming_message['data']['id']
       inc_room_id = incoming_message['data']['roomId']
+      inc_person_id = incoming_message['data']['personId']
+      
+      #check if this is a message sent by the bot 
+      if inc_person_id==BOT_PERSON_ID:
+        return '', 200
+      else:
+        
 
-      #reqest the txt of the message id
-      inc_msg_txt = api_webexTeams.messages.get(inc_msg_id).text
+        #reqest the txt of the message id
+        inc_msg_txt = api_webexTeams.messages.get(inc_msg_id).text
 
-      api_webexTeams.messages.create(inc_room_id,text='respose'+inc_msg_txt)
+        api_webexTeams.messages.create(inc_room_id,text='respose'+inc_msg_txt)
 
-      print(inc_msg_txt)
+        print(inc_msg_txt)
 
-      return '', 200
+        return '', 200
   else:
       abort(400)
 
