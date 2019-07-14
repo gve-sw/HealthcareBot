0f2425f4605647445ae448acad169ab5cd25b29a
agabdelb <abdellbar@gmail.com>
2019-07-14 16:00:22 +0200

    redploying

--- server.py
+++ server.py
@@ -41,6 +41,7 @@ messages_log = db['messages_log']
 def webhook():
   if request.method == 'POST':
       incoming_message  = request.json
+      print(incoming_message)
       requests_log.insert_many(incoming_message)
       #parse post reqest for message id and room id
       inc_msg_id  = incoming_message['data']['id']
