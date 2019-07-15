6d6816f1c8190a14a8770717d7a1f4bd09332fbe
agabdelb <abdellbar@gmail.com>
2019-07-15 18:13:23 +0200

    redploying

--- server.py
+++ server.py
@@ -86,10 +86,48 @@ def webhook():
         messages_log.insert_one(message_json)
 
         #check if message has file attched:
-        if 'files' in message_json:
+        if ('files' in message_json) and '/process' in inc_msg.text:
           #print(message_json)
           response = requests.get(message_json['files'][0],headers={"Authorization":"Bearer "+os.environ['WEBEX_TEAMS_ACCESS_TOKEN']})
-          print(response.text)
+          record_json={}
+          for record in response.text:
+            record_json={}
+            line=record.split(|)
+            record_json['field0']=line[0]
+            record_json['field1']=line[1]
+            record_json['field2']=line[2]
+            record_json['field3']=line[3]
+            record_json['field4']=line[4]
+            record_json['field5']=line[5]
+            record_json['field6']=line[6]
+            record_json['field7']=line[7]
+            record_json['field8']=line[8]
+            record_json['field9']=line[9]
+            record_json['field10']=line[10]
+            record_json['field11']=line[11]
+            record_json['field12']=line[12]
+            record_json['field13']=line[13]
+            record_json['field14']=line[14]
+            record_json['field15']=line[15]
+            record_json['field16']=line[16]
+            record_json['field17']=line[17]
+            record_json['field18']=line[18]
+            record_json['field19']=line[19]
+            record_json['field20']=line[20]
+            record_json['field21']=line[21]
+            record_json['field22']=line[22]
+            record_json['field23']=line[23]
+            record_json['field24']=line[24]
+            record_json['field25']=line[25]
+            record_json['field26']=line[26]
+            record_json['field27']=line[27]
+            record_json['field28']=line[28]
+            record_json['field29']=line[29]  
+            record_json['field30']=line[30]
+            record_json['field31']=line[31]    
+
+          print(record_json)
+
 
 
         api_webexTeams.messages.create(inc_room_id,text='respose'+response.text)
