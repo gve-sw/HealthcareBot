dc2ce8a1b616e8df9347c1cb97c3a1d6afd1f50d
agabdelb <abdellbar@gmail.com>
2019-07-15 17:18:03 +0200

    redploying

---
+++ test.py
@@ -0,0 +1,9 @@
+dict={
+        'decade': '1970s',
+        'artist': 'Debby Boone',
+        'song': 'You Light Up My Life',
+        'weeksAtOne': 10
+    }
+
+if 'decassde' in dict :
+	print('hola')
